7e7e2c5eaf576076d841c6d6ac21aae031cd08bd
taigarashi-app <taiki.naka20@icloud.com>
2026-08-12 20:17:59 +0900

    Update print statement from 'Hello' to 'Goodbye'

--- script.js
+++ script.js
@@ -947,7 +947,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   };
 
-  // 指を離したとき
+  // 指を離してたとき
   const dragEnd = () => {
     if (!isDragging) return;
     isDragging = false;
